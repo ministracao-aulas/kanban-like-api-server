d6b9c8909ee1bc71cd94d6c97a62c510755b376c
Tiago França <dev@tiagofranca.com>
2021-10-30 11:26:07 -0300

    No ramo master Mudanças a serem submetidas: 	modified:   app/Http/Controllers/Api/IndicadoresController.php 	modified:   routes/api.php

--- app/Http/Controllers/Api/IndicadoresController.php
+++ app/Http/Controllers/Api/IndicadoresController.php
@@ -7,5 +7,61 @@ use Illuminate\Http\Request;
 
 class IndicadoresController extends Controller
 {
-    //
+    public function new(Request $request)
+    {
+        $tokens = [//Esse é um exemplo. !!! É necessário uma implementação mais segura
+            'df5sdfsd54fsd6_012'  => 'abcjks879dhfkjdshfkjdshfdfskdfs',
+            'df5sdfsd54fsd6_345'  => 'cdejksdhfkjdshfkjdshfdfs7367834kdfs',
+        ];
+
+        $request->validate([
+            'vendas'    => 'required',
+            'date'      => 'required',
+            'vendedor'  => 'required',
+            'app_id'    => 'required|in:'.implode(',', array_keys($tokens)),
+            'app_token' => 'required',
+        ]);
+
+        $app_token_is_valid = $tokens[$request->app_id] === $request->app_token;
+
+        if(!$app_token_is_valid)
+        {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Indicador registrado com sucesso'
+        ], 201);
+    }
+
+    public function multi(Request $request)
+    {
+        $tokens = [//Esse é um exemplo. !!! É necessário uma implementação mais segura
+            'df5sdfsd54fsd6_012'  => 'abcjks879dhfkjdshfkjdshfdfskdfs',
+            'df5sdfsd54fsd6_345'  => 'cdejksdhfkjdshfkjdshfdfs7367834kdfs',
+        ];
+
+        $request->validate([
+            'app_id'    => 'required|in:'.implode(',', array_keys($tokens)),
+            'app_token' => 'required',
+            'vendedores.*.vendas'    => 'required',
+            'vendedores.*.date'      => 'required',
+            'vendedores.*.code'      => 'required',
+        ]);
+
+        $app_token_is_valid = $tokens[$request->app_id] === $request->app_token;
+
+        if(!$app_token_is_valid)
+        {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Indicador registrado com sucesso'
+        ], 201);
+    }
 }
